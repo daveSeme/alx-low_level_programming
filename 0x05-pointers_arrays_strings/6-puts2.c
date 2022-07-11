91aad634635ce1b1a2bfb681e67b40ebbc9b6768
Dave-t-Seme <davidluvai56@gmail.com>
2022-07-11 09:21:34 -0700

    a function that prints every other character of a string, starting with the first character, followed by a new line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ * @str: string to  print the characters from
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
