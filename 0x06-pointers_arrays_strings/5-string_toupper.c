4a89ae9b87567370668a421f1954daef826d1461
Dave-t-Seme <davidluvai56@gmail.com>
2022-07-14 04:44:24 -0700

    updating  a function that changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,44 +1,22 @@
-int check_seperators(char c);
-
+#include "main.h"
 /**
- * cap_string - a function that capitalizes all words of a string.
- * @s: An input string to capitalize letters
- * Return: pointer to s
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string
+ * Return: returns char
  */
-char *cap_string(char *s)
+
+
+char *string_toupper(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (s[i])
-{
-	if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-	s[i] -= 32;
-
-	if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-	s[i + 1] -= 32;
-	i++;
-}
-
-	return (s);
-}
-
-/**
- * check_seperators - Separators of words: space, tabulation, new line,
- * ,, ;, ., !, ?, ", (, ), {, and }
- * @c: an input character
- * Return: 1 if seperator, 0 otherwise
- */
-int check_seperators(char c)
-{
-	int i = 0;
-
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
-	'"', '(', ')', '{', '}' };
-
-	for (; i < 13; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-	if (c == seperators[i])
-		return (1);
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
 	}
-		return (0);
+
+	return (str);
 }
