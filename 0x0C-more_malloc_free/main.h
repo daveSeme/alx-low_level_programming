57d6e9a2721013b9933f12f5c8b41f16fde1eea8
Dave-t-Seme <davidluvai56@gmail.com>
2022-07-29 08:51:26 -0700

    updating the header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,1 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
